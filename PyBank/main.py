bc5a8eff2a7b4c231045c84824a0650213f6ebdd
ZeroK19 <namvanluu27@gmail.com>
2019-11-23 20:28:28 -0600

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,26 @@
-This is a printing Statement!
+# Dependencies
+import os 
+import pandas as pd
+
+
+# Name of the CSV file
+
+csv_file = 'Resources/03-Python_Homework_PyBank_Resources_budget_data.csv'
+
+budget_df = pd.read_csv(csv_file)
+
+
+
+# The total number of months included in the dataset
+
+
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "Profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+# Export Script to Local
